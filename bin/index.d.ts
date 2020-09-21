aa564adafc96aba203465ed44b6da4b2041979e4
MasatoMakino <unagiinu.g@gmail.com>
2020-09-21 16:25:56 +0900

    update : bin

--- bin/index.d.ts
+++ bin/index.d.ts
@@ -1,5 +1,20 @@
-import { Option } from "./Option";
+import {Option} from "./Option";
+
+/**
+ * @deprecated Use generateTasks
+ * @param option
+ */
 export declare function get(option: Option): {
+    staging: () => any;
+    deploy: () => any;
+    stagingDry: () => any;
+    deployDry: () => any;
+};
+/**
+ *
+ * @param option
+ */
+export declare function generateTasks(option: Option): {
     staging: () => any;
     deploy: () => any;
     stagingDry: () => any;
--- bin/index.js
+++ bin/index.js
@@ -1,11 +1,23 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.get = void 0;
+exports.generateTasks = exports.get = void 0;
 var src = require("gulp").src;
 var rsync = require("gulp-rsync");
 var path = require("path");
 var Option_1 = require("./Option");
+/**
+ * @deprecated Use generateTasks
+ * @param option
+ */
 function get(option) {
+    return generateTasks(option);
+}
+exports.get = get;
+/**
+ *
+ * @param option
+ */
+function generateTasks(option) {
     option = Option_1.OptionInitializer.init(option);
     var staging = function () {
         var opt = Option_1.OptionInitializer.getStagingOption(option);
@@ -32,7 +44,7 @@ function get(option) {
         deployDry: deployDry,
     };
 }
-exports.get = get;
+exports.generateTasks = generateTasks;
 var runRsync = function (option) {
     return src(path.resolve(option.root, "**"))
         .pipe(rsync(option))
